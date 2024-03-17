b372acbf3e32ab19b3895978c642356646a45e19
Pedro Almeida <pedroalmeidash@gmail.com>
2024-03-17 18:17:14 -0300

    Setup compose dependencies and main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,14 @@ android {
     namespace = "com.pedro.environmentreader"
     compileSdk = 33
 
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.10"
+    }
+
     defaultConfig {
         applicationId = "com.pedro.environmentreader"
         minSdk = 24
@@ -40,6 +48,15 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
+
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.foundation:foundation:1.4.3")
+    implementation("androidx.compose.material3:material3:1.1.0")
+
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/pedro/environmentreader/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.pedro.environmentreader
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text(text = "Text")
+        }
+    }
+}
